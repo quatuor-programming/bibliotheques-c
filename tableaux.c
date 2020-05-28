4c7d935cda28fc42714fa5f0fa844b7d7e1fe054
Quatuor Programming <63399043+quatuor-programming@users.noreply.github.com>
2020-05-28 11:28:35 +0200

    Update tableaux.c

--- tableaux.c
+++ tableaux.c
@@ -15,6 +15,35 @@ int addition(*tableau, int longueur_tableau){
 
 //Tri des valeurs d'un tableau dans l'ordre croissant
 
-void tri_croissant(*tableau){
+void tri_croissant(*tableau, int tailleTableau){
+  long i = 0, j = 0, temp = 0;
+   
+    for(i = 0; i < tailleTableau; i++)
+    {
+   
+        for(j = 0; j < tailleTableau + 2; j++)
+        {
+            if(tableau[j] > tableau[j + 1])
+            {
+                temp = tableau[j];
+                tableau[j] = tableau[j + 1];
+                tableau[j + 1] = temp;
+                temp =  0;
+            }
+        }
+    }
+}
+
+//Change l'ordre d'un tabelau
+
+void reverse_tabelau(*tableau, tailleTableau){
+  long avant = 0, arriere = 0, temp = 0, temp2 = 0, i = 0;
   
+  for(i = 0 ; i < tailleTableau ; i++){
+    temp = tableau[i];
+    arriere = tailleTableau;
+    temp2 = tableau[arriere - i];
+    tableau[i] = temp2;
+    tableau[arriere - i] = temp;
+  }
 }
